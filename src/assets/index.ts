b2a9d18202307127be8b28ad54cd393aee210470
Mr-Hypocrite <ym.jatin@gmail.com>
2023-01-28 13:09:23 +0530

    chore: add static assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,19 @@
 import EkashaaLogoSmallSVG from "./EkashaaLogoSmall.svg";
 import LogoPNG from "./logo.png";
+import HomeBg from "./HomeBg.webp";
+import { LegalServicesLogo } from "./LegalServicesLogo";
+import { PropertyHuntLogo } from "./PropertyHuntLogo";
+import { FinanceLogo } from "./FinanceLogo";
+import { LeasingLogo } from "./LeasingLogo";
+import { LoansLogo } from "./LoansLogo";
 
-export { EkashaaLogoSmallSVG, LogoPNG };
+export {
+    EkashaaLogoSmallSVG,
+    LogoPNG,
+    HomeBg,
+    PropertyHuntLogo,
+    LegalServicesLogo,
+    FinanceLogo,
+    LeasingLogo,
+    LoansLogo
+};
