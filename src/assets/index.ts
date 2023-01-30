1b7d9e28b2efc9f413901c6335e7c82346dee51f
Mr-Hypocrite <ym.jatin@gmail.com>
2023-01-30 21:34:24 +0530

    chore: add assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,9 +5,13 @@ import HomeBg from "./HomeBg.webp";
 import Footer_Design_Bat from "./Footer_Design_Bat.png";
 import Blog1 from "./Blog1.png";
 import Blog2 from "./Blog2.png";
-import DeveloperGPL from "./DeveloperGPL.png";
 import DeveloperLodha from "./DeveloperLodha.png";
-import DeveloperHiranandani from "./DeveloperHiranandani.png";
+import DeveloperBirlaEstates from "./DeveloperBirlaEstates.png";
+import DeveloperPiramal from "./DeveloperPiramal.png";
+import DeveloperRustomjee from "./DeveloperRustomjee.png";
+import DeveloperShapoorji from "./DeveloperShapoorji.png";
+import Management1 from "./Management1.png";
+import Management2 from "./Management2.png";
 import { LegalServicesLogo } from "./LegalServicesLogo";
 import { PropertyHuntLogo } from "./PropertyHuntLogo";
 import { FinanceLogo } from "./FinanceLogo";
@@ -27,7 +31,11 @@ export {
     Footer_Design_Bat,
     Blog1,
     Blog2,
-    DeveloperGPL,
     DeveloperLodha,
-    DeveloperHiranandani
+    DeveloperBirlaEstates,
+    DeveloperPiramal,
+    DeveloperShapoorji,
+    DeveloperRustomjee,
+    Management1,
+    Management2
 };
