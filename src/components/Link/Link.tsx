dbb7dfb85586b546315972e0a3958faa4b5d792b
Mr-Hypocrite <ym.jatin@gmail.com>
2023-02-04 01:03:00 +0530

    wip: link comp

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,6 @@
+import { css, styled } from "@src/styles";
+import { Link as RLink } from "react-router-dom";
+
+export const linkCss = css( {} );
+
+export const Link = styled( RLink, linkCss );
---
+++ src/pages/Home/sections/Connect.tsx
@@ -0,0 +1,3 @@
+export const Connect = () => {
+    return <div>Connect</div>;
+};
