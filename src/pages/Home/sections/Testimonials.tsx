86f708c401375bdd1433f18574b4b9ecb4b1d753
NiveshJ <niveshjain32@gmail.com>
2023-02-18 10:59:50 +0530

    fix: 🐛 Testimonials component

--- src/pages/Home/sections/Testimonials.tsx
+++ src/pages/Home/sections/Testimonials.tsx
@@ -2,8 +2,9 @@ import { Loader, TestimonialsCarousel } from "@components";
 import { Suspense } from "react";
 
 export const Testimonials = () => {
-    return;
-    <Suspense fallback={<Loader />}>
-        <TestimonialsCarousel />;
-    </Suspense>;
+    return (
+        <Suspense fallback={<Loader />}>
+            <TestimonialsCarousel />;
+        </Suspense>
+    );
 };
