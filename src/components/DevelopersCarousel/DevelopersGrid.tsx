17e063b1afb2f506ad9c1766c659fd2b26c29f0a
NiveshJ <niveshjain32@gmail.com>
2023-02-22 23:12:09 +0530

    wip: developer carousel

---
+++ src/components/DevelopersCarousel/DevelopersGrid.tsx
@@ -0,0 +1,50 @@
+import {
+    Lodha,
+    Piramal,
+    Rustomjee,
+    Shapoorji_Po,
+    Birla_Estates
+} from "@assets";
+import { Grid } from "../Grid";
+import { ImgContainer } from "../ImgContainer";
+import { Section } from "../Section";
+
+const developers: string[] = [
+    Lodha,
+    Piramal,
+    Birla_Estates,
+    Rustomjee,
+    Shapoorji_Po
+];
+
+export const DevelopersGrid = () => {
+    return (
+        <Section>
+            <Grid
+                css={{ width: "$full" }}
+                gap={"5"}
+                columns={{
+                    "@initial": "1",
+                    "@bp1": "3",
+                    "@bp3": "5"
+                }}
+                justify={"center"}
+                align={"center"}
+            >
+                {developers.map( ( dev, key ) => {
+                    return (
+                        <ImgContainer
+                            key={key}
+                            src={dev}
+                            alt="developer-logo"
+                            css={{
+                                width: "$full",
+                                margin: "$3 auto"
+                            }}
+                        />
+                    );
+                } )}
+            </Grid>
+        </Section>
+    );
+};
