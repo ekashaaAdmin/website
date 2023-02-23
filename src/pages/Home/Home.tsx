8dc436f74094efa184fa5197cbd848a298f80bfa
NiveshJ <niveshjain32@gmail.com>
2023-02-23 21:13:01 +0530

    chore: bg images

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,11 @@
-import { HomeBg } from "@src/assets";
+import {
+    Birla_Niyara_banner,
+    HomeBg,
+    Marina_Bay_1,
+    Marina_Bay_2,
+    Racecourse,
+    RustomjeeBanner
+} from "@src/assets";
 import { Box, Loader } from "@src/components";
 import { Contact } from "@src/components/Contact";
 import { CSS } from "@src/styles";
@@ -17,9 +24,11 @@ import {
 } from "./sections";
 
 const bgImages: string[] = [
-    "https://images.pexels.com/photos/126271/pexels-photo-126271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/2555635/pexels-photo-2555635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/4322027/pexels-photo-4322027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    Marina_Bay_2,
+    Marina_Bay_1,
+    Racecourse,
+    RustomjeeBanner,
+    Birla_Niyara_banner
 ];
 
 export const Home = () => {
