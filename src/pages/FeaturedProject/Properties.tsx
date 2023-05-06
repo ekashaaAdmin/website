faa117ac7810d21193d457c9377a6aefbad80ab6
Mr-Hypocrite <ym.jatin@gmail.com>
2023-05-06 19:13:41 +0530

    chore: add contact form on property page

--- src/pages/FeaturedProject/Properties.tsx
+++ src/pages/FeaturedProject/Properties.tsx
@@ -1,5 +1,6 @@
 import {
     Button,
+    Contact,
     FilterContainer,
     Flex,
     Grid,
@@ -30,45 +31,50 @@ export const Properties = () => {
     } );
 
     return (
-        <Section direction={"column"}>
-            <Intro />
-            <FilterContainer />
-            <Grid
-                columns={
-                    properties && properties?.length > 0 ? "propertiesGrid" : 1
-                }
-                align={"center"}
-                justify={"center"}
-            >
-                {properties && properties?.length > 0 ? (
-                    properties?.map( ( property ) => (
-                        <PropertyCard
-                            key={property._id}
-                            propertyData={property}
-                        />
-                    ) )
-                ) : (
-                    <Flex direction="column" gap="3" align="center">
-                        <Text
-                            typography={{
-                                "@initial": "dtHeading3",
-                                "@bp2": "dtHeading2"
-                            }}
-                            align="center"
-                        >
-                            No properties available for provided filter
-                            parameters.
-                        </Text>
-                        <Button
-                            variant={"generalButton"}
-                            css={{ maxWidth: "$mobileS" }}
-                            onClick={() => resetStore()}
-                        >
-                            Reset Filters
-                        </Button>
-                    </Flex>
-                )}
-            </Grid>
-        </Section>
+        <>
+            <Section direction={"column"}>
+                <Intro />
+                <FilterContainer />
+                <Grid
+                    columns={
+                        properties && properties?.length > 0
+                            ? "propertiesGrid"
+                            : 1
+                    }
+                    align={"center"}
+                    justify={"center"}
+                >
+                    {properties && properties?.length > 0 ? (
+                        properties?.map( ( property ) => (
+                            <PropertyCard
+                                key={property._id}
+                                propertyData={property}
+                            />
+                        ) )
+                    ) : (
+                        <Flex direction="column" gap="3" align="center">
+                            <Text
+                                typography={{
+                                    "@initial": "dtHeading3",
+                                    "@bp2": "dtHeading2"
+                                }}
+                                align="center"
+                            >
+                                No properties available for provided filter
+                                parameters.
+                            </Text>
+                            <Button
+                                variant={"generalButton"}
+                                css={{ maxWidth: "$mobileS" }}
+                                onClick={() => resetStore()}
+                            >
+                                Reset Filters
+                            </Button>
+                        </Flex>
+                    )}
+                </Grid>
+            </Section>
+            <Contact />
+        </>
     );
 };
