eaa80a859b3e2aaece58957eeb79248c1f0a5e16
Mr-Hypocrite <ym.jatin@gmail.com>
2024-01-06 00:19:28 +0530

    chore: add data for team

---
+++ src/data/teamData.ts
@@ -0,0 +1,40 @@
+import {
+    belaShahProfile,
+    anamProfilePicture,
+    atishProfilePicture
+} from "@src/assets";
+
+export const teamData = [
+    {
+        name: "Bela Shah",
+        profilePicture: belaShahProfile,
+        description: `Bela Shah is the founder and proprietor of Ekashaa
+        Realty. She has sold a staggering 44000 square feet of
+        premium real estate in South Mumbai worth ₹2700 million
+        (₹270 crores) in the last six years. In August 2023, she
+        topped the state in the MahaRera Real Estate
+        examination, which is the only government-recognized
+        professional certification for real estate agents in
+        Maharashtra. Since then, she has been featured in
+        several national dailies and industry-based publications.
+        A keen listener with an affable personality,
+        Bela’s intuitive understanding of client needs, deep
+        market knowledge and honest communication makes her one
+        of the most reliable voices in the
+        premium realty market.`
+    },
+    {
+        name: "Anam Shiekh",
+        profilePicture: anamProfilePicture,
+        description: `A seasoned sales professional with over a half a decade of experience in luxury real estate, 
+        Anam’s strengths are her strong interpersonal skills and communication. 
+        A telling sign of her commitment is the fact that she was the only employee at her previous organization to be promoted to a senior role during the pandemic year of 2020. 
+        Anam has been instrumental in effecting the sale of over 50000 square feet of premium real estate in Mumbai alone. 
+        She’s currently the Sales Manager at Ekashaa.`
+    },
+    {
+        name: "Atish Mane",
+        profilePicture: atishProfilePicture,
+        description: `Over the last 10 years, Atish has worked as a realty advisor with the most reputed brands in the luxury real estate industry, including Oberoi Realty, Lodha and Kalpataru. A MahaRera-certified agent, he built on his valuable expertise to lead and manage a holiday home business for an MNC that featured beachside and hillside villas in picturesque locations around Mumbai, including Alibaug, Lonavala, and Pawna. Atish’s extensive network of connections and nuanced knowledge of market offerings ensure that he can always find what the client is looking for. At Ekashaa, Atish heads the Luxury Residential and Second Homes division.`
+    }
+];
