6f1e01529bd174c370bf9ea43ad85c2ee1bd2f9b
Mr-Hypocrite <ym.jatin@gmail.com>
2024-01-06 00:19:48 +0530

    chore: map team data to cards

--- src/pages/About/sections/Management.tsx
+++ src/pages/About/sections/Management.tsx
@@ -1,5 +1,11 @@
-import { belaShahProfile } from "@src/assets";
-import { Flex, ImgContainer, Section, Text } from "@src/components";
+import { teamData } from "@src/data";
+import {
+    Flex,
+    ImgContainer,
+    Section,
+    TeamMemberCard,
+    Text
+} from "@src/components";
 
 export const Management = () => {
     return (
@@ -26,6 +32,7 @@ export const Management = () => {
             <Flex
                 center
                 direction="column"
+                gap={"6"}
                 css={{
                     mt: "$7",
                     "@mobileM": {
@@ -34,122 +41,23 @@ export const Management = () => {
                     "@bp1": {
                         width: "$full",
                         flexDirection: "column"
-                        // flexDirection: "row"
                     },
                     "@bp3": {
                         width: "998px"
                     }
                 }}
             >
-                <ImgContainer
-                    src={belaShahProfile}
-                    alt="blog-thubnail"
-                    css={{
-                        flex: 1,
-                        width: "20rem",
-                        maxHeight: "20rem",
-                        borderRadius: "50%",
-                        overflow: "hidden"
-                    }}
-                />
-                <Flex
-                    direction="column"
-                    gap="2"
-                    css={{ flex: 2, p: "$3" }}
-                    align={"flexStart"}
-                >
-                    <Text
-                        typography={{
-                            "@initial": "mbPara1",
-                            "@bp3": "dtHeading2"
-                        }}
-                        align={"center"}
-                        css={{ fontWeight: "$dtHeading1", alignSelf: "center" }}
-                    >
-                        Bela Shah
-                    </Text>
-                    <Text
-                        typography={{
-                            "@initial": "mbPara1",
-                            "@bp3": "dtPara1"
-                        }}
-                    >
-                        Bela Shah is the founder and proprietor of Ekashaa
-                        Realty. She has sold a staggering 44000 square feet of
-                        premium real estate in South Mumbai worth ₹2700 million
-                        (₹270 crores) in the last six years. In August 2023, she
-                        topped the state in the MahaRera Real Estate
-                        examination, which is the only government-recognized
-                        professional certification for real estate agents in
-                        Maharashtra. Since then, she has been featured in
-                        several national dailies and industry-based publications{" "}
-                        <br />
-                        <br />A keen listener with an affable personality,
-                        Bela’s intuitive understanding of client needs, deep
-                        market knowledge and honest communication makes her one
-                        of the most reliable voices in the
-                        premium realty market.
-                    </Text>
-                </Flex>
+                {teamData.map(
+                    ( { description, name, profilePicture }, index ) => (
+                        <TeamMemberCard
+                            name={name}
+                            description={description}
+                            profilePicture={profilePicture}
+                            key={`${name}_${index}`}
+                        />
+                    )
+                )}
             </Flex>
-            {/* <Box
-                size={"full"}
-                css={{
-                    backgroundColor: "#4CABD2"
-                }}
-            >
-                <Flex
-                    center
-                    direction="column"
-                    css={{
-                        margin: "auto",
-                        "@mobileM": {
-                            width: "$mobileS"
-                        },
-                        "@bp1": {
-                            width: "$full",
-                            flexDirection: "row-reverse"
-                        },
-                        "@bp3": {
-                            width: "998px"
-                        }
-                    }}
-                >
-                    <ImgContainer
-                        src={Management2}
-                        alt="blog-thubnail"
-                        css={{ flex: 1 }}
-                    />
-                    <Flex
-                        direction="column"
-                        gap="2"
-                        css={{ flex: 2, color: "White", p: "$3" }}
-                        align={"flexEnd"}
-                    >
-                        <Text
-                            typography={{
-                                "@initial": "mbPara1",
-                                "@bp3": "dtHeading3"
-                            }}
-                            css={{ fontWeight: "$dtHeading1" }}
-                        >
-                            Catchy Title/Heading Here
-                        </Text>
-                        <Text
-                            align={"right"}
-                            typography={{
-                                "@initial": "mbPara1",
-                                "@bp3": "dtPara1"
-                            }}
-                        >
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Praesentium reiciendis, corrupti vel
-                            reprehenderit nemo aliquid maiores suscipit. Ex,
-                            facilis consectetur?
-                        </Text>
-                    </Flex>
-                </Flex>
-            </Box> */}
         </Section>
     );
 };
